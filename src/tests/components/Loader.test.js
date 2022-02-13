24e2e6b5af040764ce0db967d20fb3dffa24d086
Myriam <my.greder@gmail.com>
2022-02-13 20:47:55 +0100

    init

--- src/tests/components/Loader.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Loader from '../../components/Loader';
-
-describe('Loader', () => {
-    it('Should render Loader', () => {
-        render(<Loader />);
-    });
-});
